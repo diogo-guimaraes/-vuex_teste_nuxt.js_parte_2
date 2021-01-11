ef6895552466508edf3a09a8fe36e2f13881aeb1
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2021-01-11 16:57:01 -0300

    refactor(store): index
    * fazendo mutação através de actions
    
    -  mudanças usados somentes das linhas 54 a 58

--- store/index.js
+++ store/index.js
@@ -16,16 +16,43 @@ export const state = () => ({
 
 })
 
+// export const mutations = {
+//   CHANGE_USER(state ,payload) {
+//     // console.log(payload.nome)
+//     state.user.nome = payload.nome
+//     state.user.senha = payload.senha
+//   },
+//   INCREMENT(state) {
+//     state.counter++
+//   },
+//   COMPLETAR_AULA(state ,payload) {
+//     state.aulasCompletas.push(payload)
+//   }
+// }
+
 export const mutations = {
-  CHANGE_USER(state ,payload) {
-    // console.log(payload.nome)
-    state.user.nome = payload.nome
-    state.user.senha = payload.senha
+  // CHANGE_USER(state ,payload) {
+  //   // console.log(payload.nome)
+  //   state.user.nome = payload.nome
+  //   state.user.senha = payload.senha
+  // },
+
+  CHANGE_USER(state, payload) {
+    state.user.nome = payload.name;
+  },
+  CHANGE_SENHA(state, payload) {
+    state.user.senha = payload.senha;
   },
   INCREMENT(state) {
     state.counter++
   },
   COMPLETAR_AULA(state ,payload) {
     state.aulasCompletas.push(payload)
+  }
+}
+
+export const actions = {
+  completarAula(context, payload){
+    context.commit("COMPLETAR_AULA", payload)
   }
 }
