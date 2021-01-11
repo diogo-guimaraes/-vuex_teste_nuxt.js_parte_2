7202c12a4e81648468d1af02a1db639ce9cce49e
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2021-01-11 16:08:10 -0300

    refactor(components):  Aluno
    * add mapState
    
    * mapeia o state e conseguirmos passar aulasCompletas e não $store.state.aulasCompletas

--- components/Aluno.vue
+++ components/Aluno.vue
@@ -1,9 +1,15 @@
 <template>
-  <p>aluno</p>
+  <p>Aluno {{aulasCompletas}}</p>
+    <!-- <p>Aluno {{$store.state.aulasCompletas}}</p> -->
 </template>
 
 <script>
+import {mapState} from "vuex";
 export default {
+  computed: {
+    // mapeia o state e conseguirmos passar aulasCompletas e não $store.state.aulasCompletas
+    ...mapState(["aulasCompletas"])
+  }
 
 }
 </script>
