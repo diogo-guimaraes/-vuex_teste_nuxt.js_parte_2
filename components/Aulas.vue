942465552b1e8e280c3200d921ce925485165fae
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2021-01-11 16:10:43 -0300

    refactor(components): Aulas
    * add ...mapMutations
    
    * para acionarmos a mutation como this.COMPLETAR_AULA(aula) e não  this.$store.commit("COMPLETAR_AULA", aula)
    
    * passa um v-for em li para montar a lista de aulas em data

--- components/Aulas.vue
+++ components/Aulas.vue
@@ -1,10 +1,48 @@
-<template>
-    <p>curso</p>
+<template>   
+    <div>
+        <p>Aulas</p>
+        <ul>
+            <li v-for="aula in aulas" :key="aula.nome">
+                <p> {{aula.nome}} | {{aula.duracao}}</p>
+                <button @click="completarAula(aula)">Completar Aulas</button>
+            </li>
+        </ul>
+    </div>
+ 
+
 </template>
 
 <script>
+import {mapMutations} from "vuex";
 export default {
-
+    data(){
+        return {            
+            aulas: [
+                {
+                    nome: "HTML e CSS",
+                    duracao: 15
+                },
+                {
+                    nome: "JavaScript",
+                    duracao: 30
+                },
+                {
+                    nome: "UX Design",
+                    duracao: 20
+                }
+            ]
+        }
+           
+    },
+    methods: {
+        ...mapMutations(["COMPLETAR_AULA"]),
+        completarAula (aula) {
+            // this.$store.commit("COMPLETAR_AULA", aula)
+            // ...mapMutations para acionarmos a mutation como this.COMPLETAR_AULA(aula) e não 
+            // this.$store.commit("COMPLETAR_AULA", aula)
+            this.COMPLETAR_AULA(aula)
+        }
+    }
 }
 </script>
 
